2bde48dac29f86519b0dd117a4b492be7d4e40b4
Gusti Hidayat <gustihidayat93@gmail.com>
2019-01-05 18:36:47 +0700

    revisi login

--- Gusti/Gusti/Classes/Module/Auth/Login/LoginViewController.swift
+++ Gusti/Gusti/Classes/Module/Auth/Login/LoginViewController.swift
@@ -10,26 +10,17 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func loginButton(_ sender: Any) {
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    @IBOutlet weak var createaccountButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
 }
--- Gusti/Gusti/Classes/Module/Auth/Register/RegisterViewController.swift
+++ Gusti/Gusti/Classes/Module/Auth/Register/RegisterViewController.swift
@@ -9,27 +9,18 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailRegisterField: UITextField!
+    @IBOutlet weak var passwordregisterField: UITextField!
+    @IBOutlet weak var confrimPasswordField: UITextField!
+    @IBOutlet weak var haveaccountLabel1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    @IBOutlet weak var registerButton: UIButton!
 }
